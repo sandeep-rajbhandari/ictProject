f1bb11f2ce80f4b388daa1ac3e6a8be17db1e955
Sandeep <sandeepawaz@gmail.com>
2015-06-21 23:48:31 +0545

    first commit

--- src/ictproject/ReportGenerator.java
+++ src/ictproject/ReportGenerator.java
@@ -6,11 +6,15 @@
 package ictproject;
 
 import com.itextpdf.text.BaseColor;
+ 
 import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
 import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import static ictproject.JDBCConnection.conn;
@@ -18,25 +22,33 @@ import java.io.FileOutputStream;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import javaapplication1.Preeti;
+import javaapplication1.file;
 
 /**
  *
  * @author Sandeep
  */
 public class ReportGenerator {
+    String encoding = "Identity-H";
+//    Font fontNormal = FontFactory.getFont(("C:/Users/Sandeep/Downloads/arialuni.ttf"), encoding,BaseFont.EMBEDDED, 16, Font.NORMAL);
+   // Font fontNormal = FontFactory.getFont(("C:/Users/Sandeep/Downloads/mangal.ttf"), encoding,BaseFont.EMBEDDED, 16, Font.NORMAL);
+    Font fontNormal = FontFactory.getFont(("C:/Users/Sandeep/Downloads/Preeti_0.ttf"), encoding,BaseFont.EMBEDDED, 16, Font.NORMAL);
+            
     public void writer(String selected){
        Document document=new Document(PageSize.A4); 
          try{
-         com.itextpdf.text.pdf.PdfWriter.getInstance(document,new FileOutputStream("hello.pdf")); 
+         com.itextpdf.text.pdf.PdfWriter.getInstance(document,new FileOutputStream("E:/hello.pdf")); 
          document.open(); 
          
+         document.add(getParagraph(selected,"janajatiAnusar"));
+         document.add(janajatiAnusar(selected));
+ 
+         document.newPage();
+
          document.add(getParagraph(selected,"pani ko sroth"));
          document.add(getPaniKoSroth(selected));
          
-          document.newPage();
-          document.add(getParagraph(selected,"pani janya rog ko bibaran"));
-          document.add(rogKoBibaran(selected));
-          
           document.newPage();
           document.add(getParagraph(selected,"actual budget"));
           document.add(actualBudget(selected));
@@ -45,29 +57,32 @@ public class ReportGenerator {
           document.add(getParagraph(selected,"expected budget"));
           document.add(actualBudget(selected));
           
-          document.newPage();
-          document.add(getParagraph(selected,"janajatiAnusar"));
-          document.add(janajatiAnusar(selected));
-         
          document.setPageSize(PageSize.A4.rotate());
          document.newPage();
          document.add(getParagraph(selected,"sauchalayKoAwasta"));
          document.add(sauchalayKoAwasta(selected));
+         
+         
+          document.newPage();
+          document.add(getParagraph(selected,"pani janya rog ko bibaran"));
+          document.add(rogKoBibaran(selected));
+          
+                  
          document.close();
          }catch(Exception e){
              System.out.println("error"+e.getMessage());
          } 
     }
     
-    private  PdfPTable getPaniKoSroth(String name){
+    private  PdfPTable getPaniKoSroth(String name)throws Exception{
         PdfPTable table = new PdfPTable(5); // 3 columns.
              table.getDefaultCell().setBackgroundColor(BaseColor.GRAY);
-           
-            table.addCell("Serial no");
-            table.addCell("Ward no");
-            table.addCell("Pani ko Sroth ko adhar");
-            table.addCell("Pokhari Count");
-            table.addCell("Remarks");
+            table.setWidths(new int[]{ 1, 1, 2, 1, 1});
+            table.addCell(getNepaliPhrase("l;=g+="));//"िस.नं."));
+            table.addCell(getNepaliPhrase("j8f g+="));//"वडा नं."));
+            table.addCell(getNepaliPhrase("kfgL >f]t ;'Sb} uPsf] cj:yf-clt w]/}–%,w]/}–$,l7s}–#,sd–@,5}g–!_"));//पानी श्रोत सुक्दै गएको अवस्था(अित धेरै–५,धेरै–४,िठकै–३,कम–२,छैन–१)"));
+            table.addCell(getNepaliPhrase("kf]v/Lsf] ;+Vof"));//पोखरीको संख्या"));
+            table.addCell(getNepaliPhrase("s}lkmot"));//कैफियत"));
             table.setHeaderRows(2);
             table.getDefaultCell().setBackgroundColor(null);
 
@@ -75,13 +90,12 @@ public class ReportGenerator {
            Statement stmt = conn.createStatement();
       String sql;
       String selected=name;
+      
       sql = "SELECT * from bidamanpanikosrot where name='"+name+"'";
-                System.out.println("sql>>>>>."+sql);
       ResultSet rs = stmt.executeQuery(sql);
 
       //STEP 5: Extract data from result set
       while(rs.next()){
-          System.out.println(">>>>>>>"+rs.getString("serialNo"));
           table.addCell(rs.getString("serialNo"));
           table.addCell(rs.getString("wardNo"));
           table.addCell(rs.getString("paniKoSrotCount"));
@@ -89,6 +103,10 @@ public class ReportGenerator {
           table.addCell(rs.getString("Remarks"));
  
       }
+      table.addCell(getNepaliPhrase("जम्मा"));
+      table.addCell("");
+      table.addCell("");
+      table.addCell((String.valueOf(getSum(name, "pokharicount","bidamanpanikosrot"))));
       table.addCell("");
 
         }  
@@ -102,12 +120,12 @@ public class ReportGenerator {
         PdfPTable table = new PdfPTable(6); // 3 columns.
             table.getDefaultCell().setBackgroundColor(BaseColor.GRAY);
           
-            table.addCell("Serial no");
-            table.addCell("Rog ko naam");
-            table.addCell("year");
-            table.addCell("year");
-            table.addCell("year");
-            table.addCell("remarks");
+            table.addCell(getNepaliPhrase("l;=g+="));//िस.नं.");
+            table.addCell(getNepaliPhrase("l;=g+="));//;"रोगको नाम");
+            table.addCell(getNepaliPhrase("aif{-@)^(÷&)_"));//"बर्ष""));//२०६९/७०)");
+            table.addCell(getNepaliPhrase("aif{-@)&)÷&!_"));
+            table.addCell(getNepaliPhrase("aif{-@)&)÷&!_"));
+            table.addCell(getNepaliPhrase("aif{-@)&)÷&!_"));
             table.setHeaderRows(1);
             table.getDefaultCell().setBackgroundColor(null);
 
@@ -139,11 +157,11 @@ public class ReportGenerator {
         PdfPTable table = new PdfPTable(5);
             table.getDefaultCell().setBackgroundColor(BaseColor.GRAY);
           
-            table.addCell("arthikBarsa");
-            table.addCell("khanePani");
-            table.addCell("sarSafai");
-            table.addCell("total");
-            table.addCell("remarks");
+            table.addCell(getNepaliPhrase("cfly{s jif{"));//artik barsa
+            table.addCell(getNepaliPhrase("vfg]kfgL-?k}of_"));//khanepani
+            table.addCell(getNepaliPhrase(";/;kmfO-?k}of_"));//sarsafai
+            table.addCell(getNepaliPhrase("hDdf"));//jamma
+            table.addCell(getNepaliPhrase("s}lkmot"));//kaifiyat
             table.setHeaderRows(1);
             table.getDefaultCell().setBackgroundColor(null);
 
@@ -174,24 +192,24 @@ public class ReportGenerator {
        PdfPTable table = new PdfPTable(8);
         table.setWidths(new int[]{ 1, 1, 2, 2, 2, 2, 2, 2});
         PdfPCell cell;
-        cell = new PdfPCell(new Phrase("S/N"));
+        cell = new PdfPCell(getNepaliPhrase("l;=g+="));//s.no
         cell.setRowspan(2);
         table.addCell(cell);
 
-        cell = new PdfPCell(new Phrase("Ward no"));
+        cell = new PdfPCell(getNepaliPhrase("j8f g+="));//ward no
         cell.setRowspan(2);
         table.addCell(cell);
-        cell = new PdfPCell(new Phrase("Janajati anusar ghar duri sankhya"));
+        cell = new PdfPCell(getNepaliPhrase("hfthftL cg';f/sf] 3/w'/Lsf] ljj/0f ;+Vof"));//जातजाती अनुसारको घरधुरीको िववरण संख्या"));
         cell.setColspan(5);
         table.addCell(cell);
-        cell = new PdfPCell(new Phrase("Remarks"));
+        cell = new PdfPCell(getNepaliPhrase("s}lkmot"));//कैिफयत"));
         cell.setRowspan(2);
         table.addCell(cell);
-        table.addCell("Dalit");
-        table.addCell("Adibasi Janajati");
-        table.addCell("Muslim");
-        table.addCell("anya");
-        table.addCell("total");
+        table.addCell(getNepaliPhrase("s}lkmot"));//दिलत"));
+        table.addCell(getNepaliPhrase("cflbjf;L÷hghftL"));//आिदवासी/जनजाती"));
+        table.addCell(getNepaliPhrase("d'lZnd"));//muslim
+        table.addCell(getNepaliPhrase("cGo"));//अन्य"));
+        table.addCell(getNepaliPhrase("hDdf"));//जम्मा"));
         try{
            Statement stmt = conn.createStatement();
       String sql;
@@ -222,42 +240,43 @@ public class ReportGenerator {
     private  PdfPTable sauchalayKoAwasta(String name) throws Exception{
         PdfPTable table = new PdfPTable(13);
         table.setWidths(new int[]{ 1, 1, 1,1,1,1,1,1,1,1,1,1,1});
-        PdfPCell cell;
-        cell = new PdfPCell(new Phrase("S/N"));
+        PdfPCell cell;						
+
+        cell = new PdfPCell(getNepaliPhrase("hDdf"));//;िस.नं."));
         cell.setRowspan(2);
         table.addCell(cell);
-        cell = new PdfPCell(new Phrase("Ward no"));
+        cell = new PdfPCell(getNepaliPhrase("hDdf"));//वडा नं."));
         cell.setRowspan(2);
         table.addCell(cell);
-        cell = new PdfPCell(new Phrase("Sauchalay ko bibaran"));
+        cell = new PdfPCell(getNepaliPhrase("zf}rfnosf ljj/0f"));//शौचालयका िववरण"));
         cell.setColspan(3);
         table.addCell(cell);
-        cell = new PdfPCell(new Phrase("Kula disa mukta gosana"));
+        cell = new PdfPCell(getNepaliPhrase("v'Nnf lb;fd\"St 3f]if0f"));//खुल्ला िदसामूक्त घोषण"));
         cell.setColspan(2);
         table.addCell(cell);
-        cell = new PdfPCell(new Phrase("Pisab alag garne ko sankya"));
+        cell = new PdfPCell(getNepaliPhrase("lk;fj cnu ug]{ u/]sf 3/w'/L ;+Vof"));//िपसाव अलग गर्ने गरेका घरधुरी संख्या"));
         cell.setRowspan(2);
         table.addCell(cell);
-        cell = new PdfPCell(new Phrase("Pisab maal garne ko sankya"));
+        cell = new PdfPCell(getNepaliPhrase("lk;fj dn k|of]u ug]{ 3/w'/L ;+Vof"));//िपसाव मल प्रयोग गर्ने घरधुरी संख्या"));
         cell.setRowspan(2);
         table.addCell(cell);
-        cell = new PdfPCell(new Phrase("Bio gas prayog garne ko sankya"));
+        cell = new PdfPCell(getNepaliPhrase("afof] Uof; rkL{ k|of]u ug]{ 3/w'/L ;+Vof"));//बायो ग्यास चर्पी प्रयोग गर्ने घरधुरी संख्या"));
         cell.setRowspan(2);
         table.addCell(cell);
-        cell = new PdfPCell(new Phrase("duwa rahit chulo prayog garne ko sankya"));
+        cell = new PdfPCell(getNepaliPhrase("w'Fjf/lxt r'Nxf] ePsf] 3/ ;+Vof"));//धुँवारिहत चुल्हो भएको घर संख्या"));
         cell.setRowspan(2);
         table.addCell(cell);
-        cell = new PdfPCell(new Phrase("duwa rahit gosana bhako na bhako"));
+        cell = new PdfPCell(getNepaliPhrase("w'Fjf/lxt j8f 3f]if0f  ePsf]÷gePsf]"));//धुँवारहित वडा घोषण  भएको/नभएको"));
         cell.setRowspan(2);
         table.addCell(cell);
-        cell = new PdfPCell(new Phrase("remarks"));
+        cell = new PdfPCell(getNepaliPhrase("s}lkmot"));
         cell.setRowspan(2);
         table.addCell(cell);
-        table.addCell("asthayi charpi sankhya");
-        table.addCell("sthayi charpi sankya");
-        table.addCell("charpi na bhako");
-        table.addCell("bhayeko/nabhayeko");
-        table.addCell("bhayeko bhaye miti");
+        table.addCell(getNepaliPhrase("c:yfoL rkL{ ;+Vof"));//अस्थायी चर्पी संख्या"));
+        table.addCell(getNepaliPhrase(":yfoL rkL{ ;+Vof"));//स्थायी चर्पी संख्या"));
+        table.addCell(getNepaliPhrase("rkL{ gePsf] ;+Vof"));//चर्पी नभएको संख्या"));
+        table.addCell(getNepaliPhrase("ePsf]÷gePsf"));//भएको/नभएको"));
+        table.addCell(getNepaliPhrase("ePsf] eP ldlt"));//bhayeko bhaye miti
         try{
            Statement stmt = conn.createStatement();
       String sql;
@@ -278,7 +297,7 @@ public class ReportGenerator {
             table.addCell(rs.getString("bioGasUse"));
             table.addCell(rs.getString("noSmokeGas"));
             table.addCell(rs.getString("noSmokeWard"));
-            table.addCell(rs.getString("remarks"));
+            table.addCell(new Phrase(new Chunk(rs.getString("remarks"),fontNormal)));
  
       }
 
@@ -292,55 +311,56 @@ public class ReportGenerator {
     }
     private Paragraph getParagraph(String name,String type){
         Paragraph paragraph=new Paragraph();
-        Paragraph para2=new Paragraph("gabisako naam::"+name);
-        System.out.println(">>>>>>>"+name);
         paragraph.setSpacingAfter(10);
 
         switch(type){
             case "pani ko sroth":{
-                Paragraph para1=new Paragraph("pani ko sroth");
+                Paragraph para1=new Paragraph("@= ljBfdfg kfgLsf >f]tx?sf] cj:yfM",fontNormal);
                 paragraph.add(para1);
                 break;
             }
             case "pani janya rog ko bibaran":{
-                Paragraph para1=new Paragraph("pani janya rog ko bibaran");
+                Paragraph para1=new Paragraph("%= kfgLhGo /f]ux?sf] ljj/0f M",fontNormal);
+                Paragraph para3=new Paragraph("uflj;:t/Lo kfgLhGo /f]ux?sf] ljj/0f :jf:Yo rf}sLaf6 jf dlxnf ;jo+;]ljsf af6 lng]",fontNormal);
                 paragraph.add(para1);
                 break;
             }
             case "actual budget":{
-              Paragraph para1=new Paragraph("jamma lagani");
-              Paragraph para3=new Paragraph("actual budget");
+              Paragraph para1=new Paragraph("# -s_ ut tLg jif{sf nflu uflj;n] vfg]kfgL tyf ;/;kmfO If]qdf 5'6fPsf] jh]6 ljj/0fM",fontNormal);
+              Paragraph para3=new Paragraph("uflj;sf] hDdf ;Defljt nufgL ? ",fontNormal);
                 paragraph.add(para1);
                 paragraph.add(para3);
                 break;  
             }
             case "expected budget":{
-                Paragraph para1=new Paragraph("expected budget");
-                Paragraph para3=new Paragraph("sambhabid budget");
+                Paragraph para1=new Paragraph("# -v_ cfufdL # jif{sf nflu uflj;sf] vfg]kfgL tyf ;/;kmfO If]qdf x'g ;Sg] ;Defljt ah]6M",fontNormal);
+                Paragraph para3=new Paragraph("uflj;sf] hDdf ;Defljt nufgL ? ",fontNormal);
                 paragraph.add(para1);
                 paragraph.add(para3);
                 break;
             }
             case "janajatiAnusar":{
-                Paragraph para1=new Paragraph("janajati anusar");
+                Paragraph para2=new Paragraph("uflj;sf] gfd MM"+converter(name),fontNormal);
+                Paragraph para1=new Paragraph("!= hfthftL cg';f/sf] 3/w'/L ljj/0fM",fontNormal);
+                
+                paragraph.add(para2);
                 paragraph.add(para1);
                 break;
             }
             case "sauchalayKoAwasta":{
-                Paragraph para1=new Paragraph("sauchalay ko awastha");
+                Paragraph para1=new Paragraph("$= zf}rfnosf] cj:yf M",fontNormal);//४. शौचालयको अवस्था ",fontNormal);
                 paragraph.add(para1);
                 break;
             }
             
         }
-        paragraph.add(para2);
         return paragraph ;
     }
-    private int getSum(String name,String field){
+    private int getSum(String name,String field,String tableName){
       try{
            Statement stmt = conn.createStatement();
       String sql;
-      sql = "SELECT sum() from  "+field+" where name="+name;
+      sql = "SELECT sum("+field+") from  "+tableName+" where name='"+name+"'";
       ResultSet rs = stmt.executeQuery(sql);
 
       //STEP 5: Extract data from result set
@@ -355,5 +375,14 @@ public class ReportGenerator {
       se.printStackTrace();
         }  
        return 0; 
+    }
+    private PdfPCell getNepaliPhrase(String word){
+        
+        return new PdfPCell(new Phrase(new Chunk(word,fontNormal)));
+    }
+    
+    private String converter(String unicodeString){
+        file.temp=new StringBuffer(unicodeString);
+        return new Preeti().converter();
     }
 }
